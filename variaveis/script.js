04f924774e9fac8b562a0151bc1f6293d5dda08c
MannuFeliix <mannufeliix@gmail.com>
2024-09-20 19:44:15 -0300

    Desafio Módulo 1

---
+++ variaveis/script.js
@@ -0,0 +1,40 @@
+console.log("Hello,world!");
+
+//Comentário para uma linha.
+/*
+Comentário de multiplas linhas.
+*/
+
+/*Variável, espaço para guardar informações.
+3 tipos:
+var : consegue reescrever e reatribuir. Ex nas linhas 26 a 30
+
+let : podemos reatribuir um valor,mas não podemos reescrever. Ex na linha 32 a 35
+
+const : não muda, tem sempre o mesmo valor. Ex linha 37 a 40
+
+Sintaxe:
+    Tipo de variável + nome da variável + = (recebe) + valor;
+
+Nomes de variáveis:
+    Não podemos usar espaços,iniciar com números,mas pode colocar número após a palavra.
+    camelCase : a a inicial da primeira palavra é minúscula e as letras iniciais demais palavras maiúsculas;
+    string é colocada dentro das aspas "texto,palavra, caractere", é um tipo de dado.
+    quando chama a variável não precisa colocar as aspas;
+*/
+//camelCase
+var nomeDoAluno = "Victoria";
+var nomeDoAluno = "Ananda";
+nomeDoAluno = "Desiree";
+
+console.log(nomeDoAluno);
+
+let novela1 = "Pé de chinesa";
+novela1 = "Teresa";
+
+console.log(novela1);
+
+// tipo de dado boolean : true (verdadeiro) e false (falso). Não se usa aspas. Sempre minúsculo
+const menorDeIdade = true;
+
+console.log(menorDeIdade);
